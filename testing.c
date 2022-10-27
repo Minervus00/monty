8a1d47c26f502f8ffecacc9ac05a8bbf333242f3
Minervus00 <fayouedraogo@protonmail.com>
2022-10-27 15:04:57 +0000

    fun

---
+++ testing.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int mainsss(void)
+{
+	char *token = "stack";
+
+	token = "queue";
+	token = "stack";
+	printf("%s\n", token);
+	
+	return (0);
+}
